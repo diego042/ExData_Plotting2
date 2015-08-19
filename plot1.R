b0e4acbb1b973b2603ff993fab48a16ebfeae27b
Diego Geronimo <diego.macger@hotmail.com>
2015-08-19 09:42:28 -0300

    changinf plot 1 to barplot

--- plot1.R
+++ plot1.R
@@ -8,17 +8,14 @@ SCC <- readRDS("Source_Classification_Code.rds")
 ##2008? Using the base plotting system, make a plot showing the total PM2.5 
 ##emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 
-y1999<-sum(subset(NEI$Emissions, subset=NEI$year==1999))
-y2002<-sum(subset(NEI$Emissions, subset=NEI$year==2002))
-y2005<-sum(subset(NEI$Emissions, subset=NEI$year==2005))
-y2008<-sum(subset(NEI$Emissions, subset=NEI$year==2008))
-
-yearsX<-c(1999,2002,2005,2008)
-yearsY<-c(y1999,y2002,y2005,y2008)
+#aggregate the data in the desired format
+totalemissions <- aggregate(Emissions ~ year,NEI, sum)
 
 png(file="plot1.png",width=480,height=480,)
-plot(x=yearsX,y=yearsY,xlab="Year",ylab="Total emissions (tons)",
-     main="US total emissions from PM2.5")
-model<-lm(yearsY~yearsX)
-abline(model)
+barplot(totalemissions$Emissions,
+        totalemissions$year,
+        xlab="Year",
+        ylab="PM2.5 Emissions (Tons)",
+        main="Total PM2.5 Emissions From All US Sources"
+)
 dev.off()
